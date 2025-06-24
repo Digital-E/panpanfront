f30231acdedcdb8a6e9785cc79763e0ddf3d31cc
SamuelBassett <samabassett@gmail.com>
2025-06-24 13:39:00 +0200

    update

--- components/home/home-slider.js
+++ components/home/home-slider.js
@@ -3,11 +3,13 @@ import styled from 'styled-components'
 
 import { gsap } from "gsap/dist/gsap";
 
-import { motion } from "framer-motion";
+import { motion, AnimatePresence } from 'framer-motion'
 
 import Link from '../link'
 
 import { useMediaQuery } from 'react-responsive';
+
+import _ from 'lodash'
 
 let Flickity = null;
 
@@ -33,15 +35,33 @@ const InnerContainer = styled.div`
   z-index: 1;
   box-sizing: border-box;
 
-    display: grid;
-    grid-template-columns: 12.5% 6.25% 12.5% 12.5% 6.25% 12.5% 12.5% 12.5% 12.5%;
-    grid-template-rows: repeat(3, 1fr);
-
   @media(max-width: 989px) {
     display: flex;
     align-items: center;
   }
 `
+
+const GridOne = styled(motion.div)`
+  height: 100%;
+  display: grid;
+  grid-template-columns: 12.5% 6.25% 12.5% 12.5% 6.25% 12.5% 12.5% 12.5% 12.5%;
+  grid-template-rows: repeat(3, 1fr);
+`
+
+const GridTwo = styled(motion.div)`
+  height: 100%;
+  display: grid;
+  grid-template-columns: 12.5% 12.5% 12.5% 12.5% 6.25% 12.5% 12.5% 6.25% 12.5%;
+  grid-template-rows: repeat(3, 1fr);
+`
+
+const GridThree = styled(motion.div)`
+  height: 100%;
+  display: grid;
+  grid-template-columns: 12.5% 12.5% 6.25% 12.5% 12.5% 12.5% 6.25% 12.5% 12.5%;
+  grid-template-rows: repeat(3, 1fr);
+`
+
 
 
 const Carousel = styled.div`
@@ -123,6 +143,32 @@ let Filters = styled.div`
     }
 `
 
+let GridDots = styled.div`
+  position: absolute;
+  right: 10px;
+  top: 50%;
+  transform: translateY(-50%);
+  z-index: 2;
+`
+
+let GridDot = styled.div`
+  width: 7px;
+  height: 7px;
+  background: black;
+  border-radius: 999px;
+  margin: 10px 0;
+  cursor: pointer;
+
+  :hover {
+    background: var(--blue);
+  }
+
+  &.active {
+    background: var(--blue);
+  }
+`
+
+
 
 let overlayVariants = {
   "visible": {
@@ -137,15 +183,15 @@ let overlayVariants = {
   }
 }
 
-let parallaxInstance = null;
-
-let parallaxTimeout = null;
+let hasScrolled = false;
 
 export default function Component({ data, allProjects, activeTags }) {
   let [all, setAll] = useState([]);
   let [prevOpen, setPrevOpen] = useState(0);
   let [overlayOpen, setOverlayOpen] = useState(false);
+  let [currentGridIndex, setCurrentGridIndex] = useState(0);
   let carouselRef = useRef();
+  let gridDots = useRef();
 
   let allRef = useRef(all);
 
@@ -188,29 +234,58 @@ let gridOneDisposition = [
     false, false, true, true, false, true, false, false, true
 ]
 
-let gridOneArray = [];
-let gridOneIndex = 0;
+let gridTwoDisposition = [
+  false, true, true, false, false, true, false, false, true,
+  true, false, true, true, false, false, true, false, false,
+  false, true, false, true, false, true, true, false, true
+]
+
+let gridThreeDisposition = [
+  false, true, false, false, true, true, false, false, true,
+  true, true, false, true, false, true, false, false, true,
+  true, false, false, true, true, false, false, true, false
+]
+
+let allGridDispositions = [
+  gridOneDisposition,
+  gridTwoDisposition,
+  gridThreeDisposition
+]
+
+let allGridArrays = []
+
+let gridIndex = 0;
+let gridArray = [];
 
   useEffect(() => {
     let allProjectsMultiply = [];
 
-
-
     for(let i = 0; i < 14; i++) {
         allProjectsMultiply.push(...allProjects)
     }
-
-    gridOneDisposition.forEach(item => {
-        if(item === true) {
-            gridOneIndex += 1;
-            gridOneArray.push(allProjectsMultiply[gridOneIndex])
-        } else {
-            gridOneArray.push(null)
-        }
-    })
     
-    // setAll([...allProjectsMultiply])
-    setAll(gridOneArray)
+
+    allGridDispositions.forEach(itemOne => {
+      gridIndex = 0;
+      gridArray = [];
+
+      itemOne.forEach(itemTwo => {
+        if(itemTwo === true) {
+          gridIndex += 1;
+          gridArray.push(allProjectsMultiply[gridIndex])
+        } else {
+            gridArray.push(null)
+        }
+      })
+
+      allGridArrays.push(gridArray)
+    })
+
+    setAll(allGridArrays)
+
+    setTimeout(() => {
+      selectDot();
+    }, 0)
 
     // if(window.innerWidth < 990) {
     //   setTimeout(() => {
@@ -236,7 +311,8 @@ let gridOneIndex = 0;
     //       initCarousel();
     //       destroyParallax();
     //   }
-    // })      
+    // })  
+
 
   }, []);
 
@@ -389,6 +465,63 @@ let gridOneIndex = 0;
 
 //   }, [activeTags])
 
+
+let TileWrapper = ({item}) => {
+  return (
+    <Tile>
+      {item !== null &&
+      <Link href={item.slug}>
+          <Image data={item.thumbnail}/>
+      </Link>}
+    </Tile>
+  )
+}
+
+let selectDot = () => {
+  Array.from(gridDots.current.children).forEach(item => item.classList.remove("active"))
+
+  Array.from(gridDots.current.children)[currentGridIndex]?.classList.add("active")
+}
+
+useEffect(() => {
+  selectDot();
+}, [currentGridIndex])
+
+let toSlide = (index) => {
+  setCurrentGridIndex(index)
+}
+
+let changeGridIndex = (e) => {
+
+  if (hasScrolled) return
+
+  if(e.deltaY > 0) {
+    if(currentGridIndex < 2) {
+      setCurrentGridIndex(currentGridIndex += 1)
+    } else {
+      setCurrentGridIndex(0)
+    }
+  } else {
+    if(currentGridIndex === 0) {
+      setCurrentGridIndex(2)
+    } else {
+      setCurrentGridIndex(currentGridIndex -= 1)
+    }
+  }
+
+  hasScrolled = true
+
+  setTimeout(() => {
+    hasScrolled = false;
+  }, 1500)
+}
+
+const modalVariants = {
+  initial: {opacity: 0, transform: "scale(0.97)"},
+  visible: { opacity: 1, transform: "scale(1)", transition: { duration: 0.7, delay: 0.7 } },
+  hidden: { opacity: 0, transform: "scale(0.97)", transition: { duration: 0.7 } }
+}
+
   return (
     <Container>
         <Filters>
@@ -397,19 +530,38 @@ let gridOneIndex = 0;
                 <div>Savoir-faire</div>
                 <div>Digital Content</div>
         </Filters>
-        <InnerContainer>
-        {all.map((item, index) => 
-            // <Island data={item} dataAll={all} index={index} toggle={() => clickIsland(index)} prevOpen={prevOpen} allProjects={allProjects} activeTags={activeTags} platform='desktop'/>
-            <Tile>
-                {
-                    item !== null &&
-                        <Link href={item.slug}>
-                            <Image data={item.thumbnail}/>
-                        </Link>                
-                }
-            </Tile>
-        )}
+        <InnerContainer className='inner-container' onWheel={(e) => changeGridIndex(e)}>
+          <AnimatePresence mode="wait">
+          {
+            all.map((itemOne, indexOne) => {
+              if(indexOne === 0 && currentGridIndex === 0) {
+                return (
+                <GridOne key={0} initial="initial" animate="visible" exit="hidden" variants={modalVariants}>
+                  {itemOne.map((itemTwo, indexTwo) => <TileWrapper item={itemTwo} />)}
+                </GridOne>
+                )}
+              if(indexOne === 1 && currentGridIndex === 1) {
+                return (
+                <GridTwo key={1} initial="hidden" animate="visible" exit="hidden" variants={modalVariants}>
+                  {itemOne.map((itemTwo, indexTwo) => <TileWrapper item={itemTwo} />)}
+                </GridTwo>
+                )} 
+              if(indexOne === 2 && currentGridIndex === 2) {
+                return (
+                <GridThree key={2} initial="hidden" animate="visible" exit="hidden" variants={modalVariants}>
+                  {itemOne.map((itemTwo, indexTwo) => <TileWrapper item={itemTwo} />)}
+                </GridThree>
+                )}                                  
+              }
+            )
+          }
+          </AnimatePresence>
         </InnerContainer>
+        <GridDots ref={gridDots}>
+          {all.map((item, index) => {
+            return <GridDot onClick={() => toSlide(index)}></GridDot>
+          })}
+        </GridDots>
     </Container>
   )
 }
